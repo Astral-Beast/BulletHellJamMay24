dbb4048357c17b48ece43b950da19827eb6202cf
unknown <workmail2159@gmail.com>
2024-05-10 12:00:12 -0600

    commit

--- src/scenes/circle_bullet.gd
+++ src/scenes/circle_bullet.gd
@@ -3,20 +3,27 @@ extends Node2D
 ### Signals
 signal Bullet_Hit
 
-### Consts
-var angular_speed = PI
-var speed = .05
+### Speed Consts
+const angular_speed = PI
+const speed = .05
 var dist
+enum {HOMING, BOUNCING}
 
+### Movement Vars
+var movement_type : String
+var target : Vector2
+var velocity : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	movement_type = HOMING
 	pass # Replace with function body.
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	homing_move(Vector2(0.0, 0.0), delta)
+	if movement_type == "homing":
+		homing_move(Vector2(0.0, 0.0), delta)
 	pass
 
 func homing_move(position_to_approach, delta):
