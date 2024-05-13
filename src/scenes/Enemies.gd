4fba793a370d948f0bbd2456d9d63b57643cb7f3
Zachary Henry <zwoh5th@gmail.com>
2024-05-12 21:42:54 -0600

    gh-29 Added an enemy script to handle bug where bullets only despawned for the foe who shot the bullet which hit the player

---
+++ src/scenes/Enemies.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_player_hit():
+	for foe in get_children():
+		foe.clear_bullets()
