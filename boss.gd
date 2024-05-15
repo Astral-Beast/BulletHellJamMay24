21ef12a6d1f440b97961d0623c78e340afac0b38
unknown <workmail2159@gmail.com>
2024-05-15 03:35:53 -0600

    gh-33 enemies now have spell cards. _on_shoot_timer is overrided for bosses, multiple shots can be called at once

--- boss.gd
+++ boss.gd
@@ -4,6 +4,7 @@ class_name Boss
 const syringe = preload("res://src/scenes/syringe_bullet.tscn")
 const diamond = preload("res://src/scenes/small_diamond_bullet.tscn")
 const circle_bullet = preload("res://src/scenes/circle_bullet.tscn")
+var spell_card
 
 enum shot_types {
 	SYRINGE,
@@ -11,15 +12,25 @@ enum shot_types {
 	CIRCLE_BULLET
 }
 
+enum spell_cards {
+	SPELL_CARD_ONE
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.shot_type = syringe
 	self.health = 1500
-	circle_shot(diamond, Enums.Shot_Movement.CONST_PAUSE_AIM)
+	self.spell_card = spell_cards.SPELL_CARD_ONE
 	pass # Replace with function body.
 
+func _on_shoot_timer_timeout():
+	match self.spell_card:
+		self.spell_cards.SPELL_CARD_ONE:
+			spell_card_one()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func spell_card_one():
+	circle_shot(diamond, Enums.Shot_Movement.CONST_PAUSE_AIM)
+	spiral_shot(syringe, Enums.Shot_Movement.CONSTANT)
