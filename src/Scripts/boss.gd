70545fb1ba2cec4e0ec4514fbdd0cef41143fe38
Zachary Henry <zwoh5th@gmail.com>
2024-05-18 19:55:20 -0600

    gh-136 Forgot to change starting spell back to big_ass_bullet

--- src/Scripts/boss.gd
+++ src/Scripts/boss.gd
@@ -49,7 +49,7 @@ func _process(delta: float) -> void:
 			progress_ratio = 1.0
 			$SpellCardTimer.start(spell_card_length)
 			
-			self.spell_card = self.spell_cards.FINAL_SPELL # BIG_ASS_BULLET should be the default
+			self.spell_card = self.spell_cards.BIG_ASS_BULLET # BIG_ASS_BULLET should be the default
 			
 			$Foe/ShootTimer.start(.05)
 			$Foe/ShootTimer2.start(.05)
