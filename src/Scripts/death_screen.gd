a401c4b74d89a0d7c28a166fe7e27f639dec2be7
unknown <workmail2159@gmail.com>
2024-05-19 23:13:13 -0600

    gh-172 added credits

--- src/Scripts/death_screen.gd
+++ src/Scripts/death_screen.gd
@@ -1,14 +1,16 @@
 extends Node2D
 signal new_game
+signal close
+const credits = preload("res://src/scenes/credits.tscn")
 var score:int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if score < 10 :
+	if score < 500000 :
 		$CanvasLayer/Score.text = "You scored %s. Your ancestors wonder where they went wrong." % score
-	elif score < 20:
+	elif score < 1000000:
 		$CanvasLayer/Score.text = "You scored %s. Your ancestors are dissapointed." % score
-	elif score < 30:
-		$CanvasLayer/Score.text = "You scored %s. Your ancestors are dissapointed." % score
+	elif score < 2500000:
+		$CanvasLayer/Score.text = "You scored %s. Your ancestors are indifferent to you." % score
 	else:
 		$CanvasLayer/Score.text = "You scored %s. Your ancestors are are smiling upon you." % score
 	pass # Replace with function body.
@@ -20,3 +22,14 @@ func _process(delta: float) -> void:
 func _on_play_again_pressed() -> void:
 	emit_signal("new_game")
 	queue_free()
+
+
+func _on_button_pressed() -> void:
+	var new_credits = credits.instantiate()
+	new_credits.connect("close", make_visible)
+	add_child(new_credits)
+	$CanvasLayer.visible = false
+
+func make_visible():
+	print("here")
+	$CanvasLayer.visible = true
